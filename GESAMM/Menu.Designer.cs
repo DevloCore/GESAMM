a67d7883f29074d6c2a9770ad3e695d66426d723
Sami Inan <inansami.05@gmail.com>
2023-12-12 15:06:14 +0100

    .

--- GESAMM/Menu.Designer.cs
+++ GESAMM/Menu.Designer.cs
@@ -28,74 +28,60 @@ namespace GESAMM
         /// </summary>
         private void InitializeComponent()
         {
-            médicamentsToolStripMenuItem = new ToolStripMenuItem();
-            consulterToolStripMenuItem = new ToolStripMenuItem();
-            ajouterToolStripMenuItem = new ToolStripMenuItem();
-            menuStrip1 = new MenuStrip();
-            DecisionToolStripMenuItem1 = new ToolStripMenuItem();
-            DecisionMedicamentMenu = new ToolStripMenuItem();
-            menuStrip1.SuspendLayout();
-            SuspendLayout();
+            this.médicamentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consulterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajouterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
             // 
             // médicamentsToolStripMenuItem
             // 
-            médicamentsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { consulterToolStripMenuItem, ajouterToolStripMenuItem });
-            médicamentsToolStripMenuItem.Name = "médicamentsToolStripMenuItem";
-            médicamentsToolStripMenuItem.Size = new Size(91, 20);
-            médicamentsToolStripMenuItem.Text = "Médicaments";
+            this.médicamentsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consulterToolStripMenuItem,
+            this.ajouterToolStripMenuItem});
+            this.médicamentsToolStripMenuItem.Name = "médicamentsToolStripMenuItem";
+            this.médicamentsToolStripMenuItem.Size = new System.Drawing.Size(112, 24);
+            this.médicamentsToolStripMenuItem.Text = "Médicaments";
             // 
             // consulterToolStripMenuItem
             // 
-            consulterToolStripMenuItem.Name = "consulterToolStripMenuItem";
-            consulterToolStripMenuItem.Size = new Size(180, 22);
-            consulterToolStripMenuItem.Text = "Consulter";
-            consulterToolStripMenuItem.Click += consulterToolStripMenuItem_Click;
+            this.consulterToolStripMenuItem.Name = "consulterToolStripMenuItem";
+            this.consulterToolStripMenuItem.Size = new System.Drawing.Size(154, 26);
+            this.consulterToolStripMenuItem.Text = "Consulter";
+            this.consulterToolStripMenuItem.Click += new System.EventHandler(this.consulterToolStripMenuItem_Click);
             // 
             // ajouterToolStripMenuItem
             // 
-            ajouterToolStripMenuItem.Name = "ajouterToolStripMenuItem";
-            ajouterToolStripMenuItem.Size = new Size(180, 22);
-            ajouterToolStripMenuItem.Text = "Ajouter";
+            this.ajouterToolStripMenuItem.Name = "ajouterToolStripMenuItem";
+            this.ajouterToolStripMenuItem.Size = new System.Drawing.Size(154, 26);
+            this.ajouterToolStripMenuItem.Text = "Ajouter";
             // 
             // menuStrip1
             // 
-            this.menuStrip1.Text = "menuStrip1";
-            this.menuStrip1.TabIndex = 2;
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.médicamentsToolStripMenuItem});
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.médicamentsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(1262, 28);
-            // 
-            // DecisionToolStripMenuItem1
-            // 
-            DecisionToolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { DecisionMedicamentMenu });
-            DecisionToolStripMenuItem1.Name = "DecisionToolStripMenuItem1";
-            DecisionToolStripMenuItem1.Size = new Size(64, 20);
-            DecisionToolStripMenuItem1.Text = "Decision";
-            // 
-            // DecisionMedicamentMenu
-            // 
-            DecisionMedicamentMenu.Name = "DecisionMedicamentMenu";
-            DecisionMedicamentMenu.Size = new Size(180, 22);
-            DecisionMedicamentMenu.Text = "Decider";
-            DecisionMedicamentMenu.Click += DecisionMedicamentMenu_Click;
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.ClientSize = new System.Drawing.Size(1262, 673);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
             this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
             this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Menu";
             this.Text = "Menu";
             this.Load += new System.EventHandler(this.Menu_Load);
             this.menuStrip1.ResumeLayout(false);
-            this.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
             this.PerformLayout();
 
         }
@@ -106,8 +92,5 @@ namespace GESAMM
         private ToolStripMenuItem consulterToolStripMenuItem;
         private ToolStripMenuItem ajouterToolStripMenuItem;
         private MenuStrip menuStrip1;
-        private ToolStripMenuItem DecisionToolStripMenuItem1;
-        private ToolStripMenuItem toolStripMenuItem2;
-        private ToolStripMenuItem DecisionMedicamentMenu;
     }
 }
